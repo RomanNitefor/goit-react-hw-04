6e21a4527f28211f6acfcfa974d2a5a9d04315e6
RomanNitefor <nitefor.roma@gmail.com>
2024-07-13 14:43:52 +0300

    change_components_1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App/App";
+import App from "./new/App/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
